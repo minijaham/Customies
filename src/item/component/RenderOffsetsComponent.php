6bba16597d29daedeced8b6f23b1327a2bd70f8f
minijaham <pikachu061125@gmail.com>
2024-11-18 11:25:55 +0900

    Test

--- src/item/component/RenderOffsetsComponent.php
+++ src/item/component/RenderOffsetsComponent.php
@@ -9,6 +9,10 @@ final class RenderOffsetsComponent implements ItemComponent {
 	private int $textureHeight;
 	private bool $handEquipped;
 
+	// public function __construct(int $textureSize) {
+	// 	$this->textureSize = $textureSize;
+	// }
+
 	public function __construct(int $textureWidth, int $textureHeight, bool $handEquipped = false) {
 		$this->textureWidth = $textureWidth;
 		$this->textureHeight = $textureHeight;
@@ -20,6 +24,30 @@ final class RenderOffsetsComponent implements ItemComponent {
 	}
 
 	public function getValue(): array {
+		// $textureSize   = $this->textureSize;
+		// $mainHandFirst = round(0.039 * 16 / $textureSize, 8);
+		// $offHandFirst  = round(0.065 * 16 / $textureSize, 8);
+		// $mainHandThird = $offHandThird = round(0.0965 * 16 / $textureSize, 8);
+
+		// return [
+		// 	"main_hand" => [
+		// 		"first_person" => [
+		// 			"scale" => [$mainHandFirst, $mainHandFirst, $mainHandFirst],
+		// 		],
+		// 		"third_person" => [
+		// 			"scale" => [$mainHandThird, $mainHandThird, $mainHandThird]
+		// 		]
+		// 	],
+		// 	"off_hand" => [
+		// 		"first_person" => [
+		// 			"scale" => [$offHandFirst, $offHandFirst, $offHandFirst],
+		// 		],
+		// 		"third_person" => [
+		// 			"scale" => [$offHandThird, $offHandThird, $offHandThird]
+		// 		]
+		// 	]
+		// ];
+
 		$horizontal = ($this->handEquipped ? 0.075 : 0.1) / ($this->textureWidth / 16);
 		$vertical = ($this->handEquipped ? 0.125 : 0.1) / ($this->textureHeight / 16);
 		$perspectives = [
